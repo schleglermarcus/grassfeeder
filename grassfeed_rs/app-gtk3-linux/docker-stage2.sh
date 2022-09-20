d3381cbd7d5db0c805783f6055f780fbfe0673ca
Marcus <schlegler_marcus@posteo.de>
2022-09-20 23:46:35 +0200

    without version number

--- grassfeed_rs/app-gtk3-linux/docker-stage2.sh
+++ grassfeed_rs/app-gtk3-linux/docker-stage2.sh
@@ -8,5 +8,5 @@ DIR=`pwd`
 test -d target || mkdir target
 docker build -t grassfeeder:stage2 -f  stage2.docker .
 
-docker cp $(docker create --name tc grassfeeder:stage2):/usr/src/gf/grassfeed_rs/target/debian/grassfeeder_0.0.4_amd64.deb target/ ; docker rm tc
+docker cp $(docker create --name tc grassfeeder:stage2):/usr/src/gf/grassfeed_rs/target/debian/grassfeeder*.deb target/ ; docker rm tc
 
