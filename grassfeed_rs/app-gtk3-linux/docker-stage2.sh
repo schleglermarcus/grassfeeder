9cdac11a5c743a835b804de6a6a8a11330198ad1
Marcus <schlegler_marcus@posteo.de>
2022-09-20 23:38:53 +0200

    transfer files via tar.gz

--- grassfeed_rs/app-gtk3-linux/docker-stage2.sh
+++ grassfeed_rs/app-gtk3-linux/docker-stage2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-mkdir target
+DIR=`pwd` 
+
+# the output file is used by the docker file
+(cd ../../ ; tar c --exclude=grassfeed_rs/target --exclude=grassfeed_rs/Cargo.lock   grassfeed_rs  |gzip --fast  >$DIR/target/gf.tar.gz )
+
+test -d target || mkdir target
 docker build -t grassfeeder:stage2 -f  stage2.docker .
 
 docker cp $(docker create --name tc grassfeeder:stage2):/usr/src/gf/grassfeed_rs/target/debian/grassfeeder_0.0.4_amd64.deb target/ ; docker rm tc
