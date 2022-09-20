a6b6e2283bd98a3bdb2c9ce53111b030fc8e2ff2
Marcus <schlegler_marcus@posteo.de>
2022-09-21 00:04:42 +0200

    create target first

--- grassfeed_rs/app-gtk3-linux/docker-stage2.sh
+++ grassfeed_rs/app-gtk3-linux/docker-stage2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DIR=`pwd` 
+test -d target || mkdir target
 
 # the output file is used by the docker file
 (cd ../../ ; tar c --exclude=grassfeed_rs/target --exclude=grassfeed_rs/Cargo.lock   grassfeed_rs  |gzip --fast  >$DIR/target/gf.tar.gz )
 
-test -d target || mkdir target
 docker build -t grassfeeder:stage2 -f  stage2.docker .
 
 docker cp $(docker create --name tc grassfeeder:stage2):/usr/src/gf/grassfeed_rs/target/debian/grassfeeder*.deb target/ ; docker rm tc
