d1b2faafc034db9c053520aef9b87292e6d14ac4
Marcus <schlegler_marcus@posteo.de>
2022-09-24 23:11:07 +0200

    desktop icon added

--- grassfeed_rs/app-gtk3-win/stage2.sh
+++ grassfeed_rs/app-gtk3-win/stage2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -rf target
 mkdir target
-(tar c --exclude=target --exclude=Cargo.lock   .  |gzip --fast  >target/cr_src.tar.gz )
+(tar c --exclude=target --exclude=Cargo.lock  ../  |gzip --fast  >target/cr_src.tar.gz )
 
-docker build -t grassfeeder:fedora1 -f fedora-stage2.docker .
+docker build -t grassfeeder:fedora2 -f fedora-stage2.docker .
 
 docker cp $(docker create --name tc grassfeeder:fedora2):/usr/src/out_package.zip target/ ; docker rm tc
 ls -l target/
