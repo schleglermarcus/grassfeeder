f40877430d4ec1082a74512c994c55bd74cb40fd
Marcus <schlegler_marcus@posteo.de>
2022-10-22 23:32:09 +0200

    debug output for systray icon loading

--- grassfeed_rs/fr_gtk/src/systray_icon.rs
+++ grassfeed_rs/fr_gtk/src/systray_icon.rs
@@ -1,18 +1,37 @@
+use crate::gtk::prelude::WidgetExt;
+use gtk::prelude::GtkMenuItemExt;
+use gtk::prelude::MenuShellExt;
+use gui_layer::abstract_ui::GuiEvents;
 use gui_layer::abstract_ui::UiSenderWrapperType;
 use libappindicator::AppIndicator;
 
-pub const ICON_PATH: &str = "/usr/share/pixmaps/grassfeeder/";
-pub const ICON2: &str = "grassfeeder-indicator2"; // grassfeeder-indicator2.png
+pub const ICON_PATH_REL: &str = "usr/share/pixmaps/grassfeeder/"; // "/usr/share/pixmaps/grassfeeder/";
+pub const FILENAME_BASE: &str = "grassfeeder-indicator2";
 
+//	libappindicator  is somewhat broken. It requests absolute paths for the Icons.
+//  This works with the regular *.deb distribution
+//  But it fails with   AppImage distribution
 pub fn create_systray_icon_3(g_ev_se: UiSenderWrapperType, app_url: String) -> AppIndicator {
-    use crate::gtk::prelude::WidgetExt;
-    use gtk::prelude::GtkMenuItemExt;
-    use gtk::prelude::MenuShellExt;
-    use gui_layer::abstract_ui::GuiEvents;
-    // trace!("TRAY3: {}  {}", ICON_PATH, ICON2);
+    let icon_path_base;
+
+    let icon_filename1 = format!("{}{}.png", ICON_PATH_REL, FILENAME_BASE);
+    if std::path::Path::new(&icon_filename1).exists() {
+        icon_path_base = ICON_PATH_REL.to_string();
+        debug!("Icon1 {} found! ", icon_filename1,);
+    } else {
+        warn!("Icon {} missing! ", icon_filename1,);
+        let icon_filename2 = format!("/{}{}.png", ICON_PATH_REL, FILENAME_BASE);
+        icon_path_base = format!("/{}", ICON_PATH_REL);
+        if !std::path::Path::new(&icon_filename2).exists() {
+            debug!("Icon2 {} found! ", icon_filename2);
+        } else {
+            error!("Icon {} missing!", &icon_filename2);
+        }
+    }
     let mut indicator = AppIndicator::new(app_url.as_str(), "");
-    indicator.set_icon_theme_path(ICON_PATH);
-    indicator.set_icon(ICON2);
+    indicator.set_icon_theme_path(&icon_path_base);
+    indicator.set_icon(FILENAME_BASE);
+
     let mut menu = gtk::Menu::new();
     let mi1 = gtk::MenuItem::with_label(&t!("SYSTRAY_CMD_SHOW_WINDOW"));
     let se_w1 = g_ev_se.clone(); //  EvSenderWrapper(g_ev_se.clone());
