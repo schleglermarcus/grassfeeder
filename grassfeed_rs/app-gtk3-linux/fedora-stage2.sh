55c4a4d33949c3d7b3fca87d89ad3b4a322db7c7
Marcus <schlegler_marcus@posteo.de>
2022-11-08 13:07:09 +0100

    using fedora35

--- grassfeed_rs/app-gtk3-linux/fedora-stage2.sh
+++ grassfeed_rs/app-gtk3-linux/fedora-stage2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 DIR=`pwd`
 VERSION=`cat Cargo.toml  |grep "^version"  |sed -e "s/.*= \"//" -e "s/\"//"`
 rm -rf target
@@ -11,23 +10,11 @@ mkdir target
 
 docker build -t grassfeeder:fedora-stage2 -f fedora-stage2.docker .
 
+docker cp $(docker create --name tc grassfeeder:fedora-stage2):/usr/src/grassfeed_rs/app-gtk3-linux/gf.AppImage target/ ; docker rm tc
+mv target/gf.AppImage  target/grassfeeder-${VERSION}-fedora35.AppImage
 
-
-
+docker cp $(docker create --name tc grassfeeder:fedora-stage2):/usr/src/grassfeed_rs/target/gf.rpm target/ ; docker rm tc
+mv target/gf.rpm  target/grassfeeder-${VERSION}-fedora35.rpm
 
 #docker cp $(docker create --name tc grassfeeder:debian-stage2):/usr/src/grassfeed_rs/target/gf.deb target/ ; docker rm tc
 #mv target/gf.deb  target/grassfeeder-${VERSION}_x86_64.deb
-
-docker cp $(docker create --name tc grassfeeder:fedora-stage2):/usr/src/grassfeed_rs/app-gtk3-linux/gf.AppImage target/ ; docker rm tc
-mv target/gf.AppImage  target/grassfeeder-${VERSION}-fedora33.AppImage
-
-docker cp $(docker create --name tc grassfeeder:fedora-stage2):/usr/src/grassfeed_rs/target/gf.rpm target/ ; docker rm tc
-mv target/gf.rpm  target/grassfeeder-${VERSION}-fedora33.rpm
-
-
-
-# docker cp $(docker create --name tc grassfeeder:fedora2):/usr/src/out_package.zip target/ ; docker rm tc
-#ls -l target/
-
-
-
