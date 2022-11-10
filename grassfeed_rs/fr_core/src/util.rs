9c67627e15f297666f3979963ace6c89bdcd27a1
Marcus <schlegler_marcus@posteo.de>
2022-11-10 11:37:12 +0100

    special chars handling, set rpm name

--- grassfeed_rs/fr_core/src/util.rs
+++ grassfeed_rs/fr_core/src/util.rs
@@ -218,7 +218,7 @@ pub fn filter_by_iso8859_1(input: &str) -> (String, bool) {
 ///  https://www.w3schools.com/charsets/ref_html_entities_a.asp
 pub fn remove_invalid_chars_from_input(inp: String) -> String {
     let mut ret = inp;
-    ret = ret.replace(&['\"', '\n', '\'', '\"'][..], "");
+    ret = ret.replace(&['\"', '\n', '\"'][..], "");
     ret = ret.replace("&#38;", "&");
     ret = ret.replace("&#038;", "&");
     ret = ret.replace("&#039;", "\"");
@@ -234,9 +234,11 @@ pub fn remove_invalid_chars_from_input(inp: String) -> String {
     ret = ret.replace("&#153;", " - ");
     ret = ret.replace("&#156;", " - ");
     ret = ret.replace("&#157;", " Š ");
+    ret = ret.replace("&#163;", "£");
     ret = ret.replace("&#164;", " ");
     ret = ret.replace("&#166;", " ... ");
     ret = ret.replace("&#190;", "¾");
+    ret = ret.replace("&#194;", "Â");
     ret = ret.replace("&#220;", "Ü");
     ret = ret.replace("&#223;", "ß");
     ret = ret.replace("&#226;", "\"");
@@ -291,7 +293,6 @@ mod t {
     use crate::util::remove_invalid_chars_from_input;
     // use std::fs::File;
     // use std::io::Write;
-
 
     //cargo watch -s "cargo    test  util::util_fetch_test::sanitize_input   --lib  -- --exact "
     #[test]
--- grassfeed_rs/fr_core/tests/t_messages.rs
+++ grassfeed_rs/fr_core/tests/t_messages.rs
@@ -1,3 +1,4 @@
+use fr_core::db::message::decompress;
 use fr_core::db::message::MessageRow;
 use fr_core::db::messages_repo::IMessagesRepo;
 use fr_core::db::messages_repo::MessagesRepo;
@@ -30,6 +31,9 @@ fn feed_text_to_entries_naturalnews() {
     assert_eq!(new_list.len(), 30);
     assert_eq!(new_list[1].entry_src_date, 1655877600);
     assert_eq!(new_list[2].entry_src_date, 1655877600);
+    // new_list.iter().enumerate().for_each(|(n, le)| {        debug!("{} \t {}", n, decompress(&le.title));    });
+    assert_eq!( decompress (&new_list.get(10).unwrap().title),
+	   "White House press secretary ripped for claiming it'll take days to count ballots so many winners won't be immediately known".to_string()   );
 }
 
 //RUST_BACKTRACE=1 cargo watch -s "cargo test  downloader::messages::t_::t_strange_datetime_recover    --lib -- --exact --nocapture "
@@ -40,7 +44,7 @@ fn t_strange_datetime_recover() {
     let mtext = std::fs::read_to_string(filename).unwrap();
     let (mut new_list, _ts_created, _err): (Vec<MessageRow>, i64, String) =
         feed_text_to_entries(mtext.clone(), 5, "some-url".to_string());
-    debug!("prev:   {:?}", new_list[0].entry_src_date);
+    // debug!("prev:   {:?}", new_list[0].entry_src_date);
     let o_msg = strange_datetime_recover(&mut new_list, &mtext);
     assert!(o_msg.is_none());
     assert_eq!(new_list[0].entry_src_date, 1655935140)
@@ -55,7 +59,6 @@ fn feed_text_to_entries_local() {
         feed_text_to_entries(contents.clone(), 5, "some-url".to_string());
     assert_eq!(new_list.len(), 2);
     assert_eq!(ts_created, 1636573888);
-
     let msgrepo = MessagesRepo::new_in_mem();
     msgrepo.get_ctx().create_table();
     let msgrepo_r: Rc<RefCell<dyn IMessagesRepo>> = Rc::new(RefCell::new(msgrepo));
@@ -79,7 +82,6 @@ static TEST_SETUP: Once = Once::new();
 fn setup() {
     TEST_SETUP.call_once(|| {
         let _r = logger_config::setup_fern_logger(logger_config::QuietFlags::Controller as u64);
-        //   unzipper::unzip_some();
-        debug!("UNZIPPED: {}", unzipper::unzip_some());
+        unzipper::unzip_some();
     });
 }
