1cdd1c7ba5c6323aed0d895877ec227b98eda2c3
Marcus <schlegler_marcus@posteo.de>
2022-11-12 22:52:34 +0100

    debi repo, pt4

--- debian/update.sh
+++ debian/update.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # https://linuxconfig.org/easy-way-to-create-a-debian-package-and-local-package-repository 
 #
+
+
+
+export REPREPRO_BASE_DIR=`pwd`
+
+DEBS=`find . -name "*.deb" |xargs`
+
+reprepro includedeb grassfeeder $DEBS
+
 # dpkg-scanpackages . | gzip -c9  > Packages.gz
-dpkg-scanpackages -m pool | gzip  > dists/grassfeeder/Packages.gz
+# dpkg-scanpackages -m pool | gzip  > dists/grassfeeder/Packages.gz
 
