4d1946806c5e155778b995f35bda4175c03a572a
Marcus <schlegler_marcus@posteo.de>
2022-11-12 03:27:30 +0100

    pt3

--- debian/update.sh
+++ debian/update.sh
@@ -2,5 +2,5 @@
 # https://linuxconfig.org/easy-way-to-create-a-debian-package-and-local-package-repository 
 #
 # dpkg-scanpackages . | gzip -c9  > Packages.gz
-dpkg-scanpackages . | gzip  > Packages.gz
+dpkg-scanpackages -m pool | gzip  > dists/grassfeeder/Packages.gz
 
