d9b6513ee6c3bc7cc353f98204f66ffe117fbe29
Marcus <schlegler_marcus@posteo.de>
2022-11-12 23:56:45 +0100

    pt5

--- debian/README.md
+++ debian/README.md
@@ -4,4 +4,5 @@
 `wget -P /usr/share/keyrings  https://github.com/schleglermarcus/grassfeeder/raw/main/debian/grassfeeder-archive-keyring.gpg  `
 
 
-deb [arch=all signed-by=/usr/share/keyrings/grassfeeder-archive-keyring.gpg] https://raw.githubusercontent.com/schleglermarcus/grassfeeder/debian/master grassfeeder main
+
+`deb [arch=all signed-by=/usr/share/keyrings/grassfeeder-archive-keyring.gpg] https://raw.githubusercontent.com/schleglermarcus/grassfeeder/debian/ grassfeeder main`
--- debian/update.sh
+++ debian/update.sh
@@ -2,14 +2,24 @@
 # https://linuxconfig.org/easy-way-to-create-a-debian-package-and-local-package-repository 
 #
 
-
-
 export REPREPRO_BASE_DIR=`pwd`
 
 DEBS=`find . -name "*.deb" |xargs`
 
+reprepro --delete clearvanished 
 reprepro includedeb grassfeeder $DEBS
 
 # dpkg-scanpackages . | gzip -c9  > Packages.gz
 # dpkg-scanpackages -m pool | gzip  > dists/grassfeeder/Packages.gz
+#  DEBS    
 
+#
+#apt-get update   --print-uris
+#
+# apt-get will haben: 
+# https://raw.githubusercontent.com/schleglermarcus/grassfeeder/main/debian/dists/grassfeeder/InRelease
+# auf github:
+# https://raw.githubusercontent.com/schleglermarcus/grassfeeder/main/debian/dists/grassfeeder/InRelease
+
+# https://raw.githubusercontent.com/schleglermarcus/grassfeeder/main/debian/dists/grassfeeder/InRelease
+# https://raw.githubusercontent.com/schleglermarcus/grassfeeder/main/debian/dists/grassfeeder/InRelease
