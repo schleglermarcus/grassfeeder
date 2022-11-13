65d6452f9090a8c754503cd2eb6f3a5409840e1a
Marcus <schlegler_marcus@posteo.de>
2022-11-13 03:46:18 +0100

    another test version

--- debian/README.md
+++ debian/README.md
@@ -1,8 +1,9 @@
+## Package Source for  Debian / Ubuntu / Mint
 
-## Add debian repository
 
+1. Add the key file to your local  mint / debian : 
 `wget -P /usr/share/keyrings  https://github.com/schleglermarcus/grassfeeder/raw/main/debian/grassfeeder-archive-keyring.gpg  `
 
 
-
+2. Add this repository to the sources list, either via   Settings / Package Sourcs, or via   /etc/apt/sources.list.d/additional-repositories.list
 `deb [arch=all signed-by=/usr/share/keyrings/grassfeeder-archive-keyring.gpg] https://raw.githubusercontent.com/schleglermarcus/grassfeeder/debian/ grassfeeder main`
--- debian/update.sh
+++ debian/update.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
-# https://linuxconfig.org/easy-way-to-create-a-debian-package-and-local-package-repository 
-#
+#  Update the repository tree, with the new *.deb
 
 export REPREPRO_BASE_DIR=`pwd`
+test -d dists && mv dists ../grassfeed_rs/target/dists.old
+test -d db && mv db ../grassfeed_rs/target/db.old
 
 DEBS=`find . -name "*.deb" |xargs`
-
-test -d dists && mv dists dists.old
-test -d db && mv db db.old
-
 reprepro --delete clearvanished 
 reprepro includedeb grassfeeder $DEBS
 
-# dpkg-scanpackages . | gzip -c9  > Packages.gz
-# dpkg-scanpackages -m pool | gzip  > dists/grassfeeder/Packages.gz
-#  DEBS    
 
-
-
-
-
-# apt-ftparchive --md5 --sha256 release .  > Release 
-# gpg --digest-algo SHA256 --armor --output Release.gpg --detach-sign Release
-# gpg --digest-algo SHA256 --clearsign --output InRelease Release
 
 # https://unix.stackexchange.com/questions/387053/debian-9-apt-and-gpg-error-inrelease-the-following-signatures-were-inva
 # Adjust the personal-digest-preferences and personal-cipher-preferences 
 # in $HOME/.gnupg/gpg.conf to eliminate SHA-1 from one's GPG preferences. 
 # This prevents the problem coming back with new keys.
 
+
+
+# apt-ftparchive --md5 --sha256 release .  > Release 
+# gpg --digest-algo SHA256 --armor --output Release.gpg --detach-sign Release
+# gpg --digest-algo SHA256 --clearsign --output InRelease Release
 
 
